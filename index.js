4fcc126b47b49e1dd495926ef87bcfbb71b29d4f
geoffrey woodcock <theatomproject@gmail.com>
2018-01-14 19:51:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function destructivelyRemoveLastKitten(name){
   kittens.pop(name)
   return kittens
 }
+kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveFirstKitten(name){
+  kittens.push(name)
+  return kittens
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function appendKitten(name){
+  return kittens[...kittens, name]
+}
+kittens = ['Milo', 'Otis', 'Garfield']
+function prependKitten(name){
+
+}
